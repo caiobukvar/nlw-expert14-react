fe4298e348fb51e0cd07023c8bd7d9d07bf7e7ac
Caio <bukvar2009@gmail.com>
2024-02-06 10:25:32 -0300

    adicionando radix-ui + date-fns

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+const note = {
+  createdAt: new Date(),
+  content: "Hello",
+};
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -19,9 +24,7 @@ export function App() {
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
       </div>
     </div>
   );
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,16 +1,41 @@
-export function NoteCard() {
+import * as Dialog from "@radix-ui/react-dialog";
+
+interface NoteCardProps {
+  note: {
+    createdAt: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 text-left focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+    <Dialog.Root>
+      {/* trigger vai agir como botão */}
+      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 text-left focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {note.createdAt.toISOString()}
+        </span>
 
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo possimus
-        tempora deleniti eligendi minus dignissimos labore praesentium neque sit
-        quis. Esse saepe nam corporis officia soluta aliquid, minima facere
-        fuga.
-      </p>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/60">
+          {/* ! usa -translate para ir para o lado oposto e centralizar */}
+          <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+            {/* flex-1 > ocupa o max de espaco possivel, pode reduzir caso necessario se outro elemento precisar de espaço */}
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className="text-sm font-medium text-slate-300">
+                há {note.createdAt.toISOString()} dias atrás
+              </span>
+
+              <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+            </div>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
