e8d76ae42332d247be10be227722f38c32979fd8
Caio <bukvar2009@gmail.com>
2024-02-06 10:32:44 -0300

    feat: add lucide-reacty

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,4 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 interface NoteCardProps {
   note: {
@@ -13,7 +15,10 @@ export function NoteCard({ note }: NoteCardProps) {
       {/* trigger vai agir como botão */}
       <Dialog.Trigger className="rounded-md flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 text-left focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-300">
-          {note.createdAt.toISOString()}
+          {formatDistanceToNow(note.createdAt, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
 
         <p className="text-sm leading-6 text-slate-400">{note.content}</p>
@@ -24,15 +29,31 @@ export function NoteCard({ note }: NoteCardProps) {
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/60">
           {/* ! usa -translate para ir para o lado oposto e centralizar */}
-          <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+          {/* overflow-hidden faz com que o botão dentro do modal não passe do tamanho da div */}
+          <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none overflow-hidden">
             {/* flex-1 > ocupa o max de espaco possivel, pode reduzir caso necessario se outro elemento precisar de espaço */}
             <div className="flex flex-1 flex-col gap-3 p-5">
               <span className="text-sm font-medium text-slate-300">
-                há {note.createdAt.toISOString()} dias atrás
+                {formatDistanceToNow(note.createdAt, {
+                  locale: ptBR,
+                  addSuffix: true,
+                })}
               </span>
 
               <p className="text-sm leading-6 text-slate-400">{note.content}</p>
             </div>
+
+            <button
+              type="button"
+              // group deixa você condicionar os filhos a funcionarem separadamente (como abaixo, underline somente no span e nao no conteúdo inteiro.)
+              className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none group"
+            >
+              Deseja{" "}
+              <span className="text-red-400 group-hover:underline">
+                apagar esta nota
+              </span>
+              ?
+            </button>
           </Dialog.Content>
         </Dialog.Overlay>
       </Dialog.Portal>
